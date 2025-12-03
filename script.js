bd16db53ea39625f6143357237eb203cc0cd28ad
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2025-12-03 16:49:00 +0530

    Add dynamic task creation and completion UI
    Implemented dynamic task card creation in script.js, allowing users to add tasks with priority and timestamp. Added UI feedback for completed tasks, including background and text styling in style.css, and updated input/select element attributes in index.html for better accessibility and data handling.

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
 const slider = document.querySelector('.progressbar input');
-const giventask = document.querySelector('.addtasks input')
+const giventask = document.querySelector("#giventask")
 const prio = document.querySelector('.addtasks select')
 const addtask = document.querySelector('.addtasks button')
+const elbeforecard = document.querySelector('.addedtasksouter')
 const tasks = []
 let i = 0
+
 
 slider.addEventListener('input', e => {
     e.target.value = e.target.dataset.value;
 });
 slider.dataset.value = slider.value;
 
-addtask.addEventListener("click", ()=>{
-    tasks[i] = {
-        task: giventask,
-        priority: prio,
-        time: Date.now()
-    }
+function taskcard(task,pri,time){
+    elbeforecard.innerHTML += `<div class="addedtask">
+                <div class="check">
+                    <input type="checkbox" name="" class="status">
+                </div>
+                <div class="taskandtime">
+                    <div class="task">${task}</div>
+                    <div class="timecreated">${time}</div>
+                </div>
+                <div class="tools">
+                    <select name="" id="">
+                        <option value="Low" ${pri === "Low"?"selected":""}>Low</option>
+                        <option value="Medium" ${pri === "Medium"?"selected":""}>Medium</option>
+                        <option value="High" ${pri === "High"?"selected":""}>High</option>
+                    </select>
+                    <button class="edit"><i class="ri-pencil-line"></i></button>
+                    <button class="delete"><i class="ri-delete-bin-line"></i></button>
+                </div>
+            </div>` 
+}
 
-    console.log(tasks[i])
-    i++
+addtask.addEventListener("click", ()=>{
+    tasks.push({
+        task: giventask.value,
+        priority: prio.value,
+        time: Date.now(),
+        status: "pending"
+    })
+    taskcard(tasks[i].task,tasks[i].priority,tasks[i].time)
+    console.log(tasks[i]); i++
+    giventask.value = ""
 })
+
+document.addEventListener("change", (e) => {
+    if (e.target.matches(".addedtask .check input[type='checkbox']")) {
+
+        const card = e.target.closest(".addedtask"); // ← finds that specific card
+
+        if (e.target.checked) {
+            card.classList.add("completed");
+            
+        } else {
+            card.classList.remove("completed");
+        }
+    }
+});
+
+
+
